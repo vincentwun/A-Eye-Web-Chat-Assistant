be5fd713dd6aa5f9497321e9c30fdda740a2e2d3
vincentwun <vincentwan1110@gmail.com>
2025-08-24 08:31:57 +0800

    readme updated

--- gcp/gcloud/1_setup_env.sh
+++
@@ -1,9 +0,0 @@
-# Setup env
-export FUNCTION_NAME="gemini-proxy-function"
-export REGION="us-central1"
-export PROJECT_ID="api-gateway-gemini-proxy"
-export NODE_RUNTIME="nodejs20"
-export API_ID="gemini-proxy-api"
-export API_CONFIG_ID="gemini-proxy-config-v1"
-export GATEWAY_ID="gemini-proxy-gateway"
-export BILLING_ACCOUNT_ID="YOUR_BILLING_ACCOUNT_ID" # Replace with your billing account ID
